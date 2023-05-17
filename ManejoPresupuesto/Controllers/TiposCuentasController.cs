7618e00bfe9b57c3e39f9da7ec8305b61a6b574f
portobuj <portbuj@gmail.com>
2023-05-17 19:56:51 +0200

    Agregar archivos de proyecto.

---
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManejoPresupuesto.Controllers
+{
+    public class TiposCuentasController:  Controller
+    {
+        public IActionResult Crear()
+        {
+            return View();
+        }
+    }
+}
