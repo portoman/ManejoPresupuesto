abb69ef999b9d7a57d6239609b600683df347112
portobuj <portbuj@gmail.com>
2023-05-18 20:13:20 +0200

    Avance

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -6,26 +6,41 @@ using Microsoft.Data.SqlClient;
 
 namespace ManejoPresupuesto.Controllers
 {
-    public class TiposCuentasController:  Controller
+    public class TiposCuentasController : Controller
     {
         private readonly IRepositorioTiposCuentas repositorioTiposCuentas;
 
-        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas) {
+        public IServicioUsuarios servicioUsuarios { get; }
+
+        public TiposCuentasController(IRepositorioTiposCuentas repositorioTiposCuentas, IServicioUsuarios servicioUsuarios)
+        {
             this.repositorioTiposCuentas = repositorioTiposCuentas;
+            this.servicioUsuarios = servicioUsuarios;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            TipoCuenta tipoCuenta = new TipoCuenta();
+            tipoCuenta.UsuarioId = servicioUsuarios.ObtenerUsuarioId();
+
+            IEnumerable<TipoCuenta> tiposCuentas = await repositorioTiposCuentas.Obtener(tipoCuenta.UsuarioId);
+            return View(tiposCuentas);
+
         }
         public IActionResult Crear()
         {
+
             return View();
         }
 
         [HttpPost]
         public async Task<IActionResult> Crear(TipoCuenta tipoCuenta)
         {
-            if(!ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
                 return View(tipoCuenta);
             }
-            tipoCuenta.UsuarioId = 1;
+            tipoCuenta.UsuarioId = servicioUsuarios.ObtenerUsuarioId();
 
             bool yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(tipoCuenta.Nombre, tipoCuenta.UsuarioId);
 
@@ -36,14 +51,14 @@ namespace ManejoPresupuesto.Controllers
             }
 
             await repositorioTiposCuentas.Crear(tipoCuenta);
-            return View();
+            return RedirectToAction("Index");
         }
 
         public async Task<IActionResult> VerificarExisteTipoCuenta(string nombre)
         {
-            int usuarioId = 1;
+            int usuarioId = servicioUsuarios.ObtenerUsuarioId();
             var yaExisteTipoCuenta = await repositorioTiposCuentas.Existe(nombre, usuarioId);
-            if(yaExisteTipoCuenta)
+            if (yaExisteTipoCuenta)
             {
                 return Json($"El nombre {nombre} ya existe");
             }
