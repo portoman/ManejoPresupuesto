7d20d33ed993d7d8307c9976150a0a1ad93fb440
portobuj <portbuj@gmail.com>
2023-05-21 20:53:29 +0200

    Avance

--- ManejoPresupuesto/Controllers/TiposCuentasController.cs
+++ ManejoPresupuesto/Controllers/TiposCuentasController.cs
@@ -123,6 +123,9 @@ namespace ManejoPresupuesto.Controllers
             return Json(true);
         }
 
-
+        public async Task<IActionResult> Ordenar([FromBody] int[] ids)
+        {
+            return Ok();
+        }
     }
 }
